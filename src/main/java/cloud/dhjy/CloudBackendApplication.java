18b8989f4459c4babe2e04e7461f2ede6218b281
dhjygit <liu2211402141@qq.com>
2022-03-14 23:55:15 +0800

    first commit

---
+++ src/main/java/cloud/dhjy/CloudBackendApplication.java
@@ -0,0 +1,11 @@
+package cloud.dhjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBackendApplication.class, args);
+    }
+}
